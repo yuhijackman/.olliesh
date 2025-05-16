822da9a2d5de5410854d78219a6553f045be2d40
Yuhi Jackman <yuuhiandlisa@gmail.com>
2025-05-16 16:47:00 +0900

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,11 @@
+
+# Alias added by add_alias function on Tue May 13 09:47:21 JST 2025
+alias c='code .'
+
+# Alias added by add_alias function on Wed May 14 22:41:38 JST 2025
+alias gq='ghq_cd'
+
+alias gr='git reset --soft HEAD^'
+
+# Alias added by add_alias function on Thu May 15 18:37:36 JST 2025
+alias pomo='pomo_set'
